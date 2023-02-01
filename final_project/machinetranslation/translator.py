2ebbd5f0c911be814ce2a0edd7bd4915339a26ca
HamzaDamouh <damouhamza11@gmail.com>
2023-02-01 15:07:25 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -12,27 +12,24 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 
 authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
+language_translator = LanguageTranslatorV3(version='2018-05-01',
+                                           authenticator=authenticator)
 language_translator.set_service_url(url)
 
 
-
-
 def english_to_french(english_text):
-  ''' Translate a given expression from english to french '''
-  
-  translation = language_translator.translate(text=english_text,model_id="en-fr").get_result()
-  french_text = translation["translations"][0]["translation"]
-  return french_text
+    ''' Translate a given expression from english to french '''
 
-
+    translation = language_translator.translate(text=english_text,
+                                                model_id="en-fr").get_result()
+    french_text = translation["translations"][0]["translation"]
+    return french_text
 
 
 def french_to_english(french_text):
-  ''' Translate a given expression from french to english '''
+    ''' Translate a given expression from french to english '''
 
-  translation = language_translator.translate(text=french_text,model_id="fr-en").get_result()
-  english_text = translation["translations"][0]["translation"]
-  return english_text
-
-print(french_to_english("Roi"))
+    translation = language_translator.translate(text=french_text,
+                                                model_id="fr-en").get_result()
+    english_text = translation["translations"][0]["translation"]
+    return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -2,24 +2,27 @@ from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
 
-
 app = Flask("Web Translator")
+
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    result = translator.english_to_french(textToTranslate)
-	  return "Translated text to French: " + result
+        textToTranslate = request.args.get('textToTranslate')
+        result = translator.english_to_french(textToTranslate)
+        return "Translated text to French: " + result
+
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    result = translator.french_to_english(textToTranslate)
-	  return "Translated text to English: " + result
+        textToTranslate = request.args.get('textToTranslate')
+        result = translator.french_to_english(textToTranslate)
+        return "Translated text to English: " + result
+
 
 @app.route("/")
 def renderIndexPage():
-    return render_template("index.html")
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+        app.run(host="0.0.0.0", port=8080)
